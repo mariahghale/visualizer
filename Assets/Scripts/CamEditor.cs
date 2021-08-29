a9b031697a908533576501517e7b5de53c9c8524
mariah <mariahghale@gmail.com>
2021-08-29 10:26:08 -0700

    Implements Camera GUI Reactions
    - gets the camera editing mode working
    - removed the fov axis switcher since it wasn't a simple match up in the scripting API

--- Assets/Scripts/CamEditor.cs
+++ Assets/Scripts/CamEditor.cs
@@ -6,16 +6,55 @@ using UnityEngine.UI;
 
 public class CamEditor : BaseEditor
 {
+    public Dropdown projDropdown;
+    public Slider fovSlider;
+    public InputField nearClipEdit;
+    public InputField farClipEdit;
+
+    private GameObject camObj;
+    private Camera cam;
+    private string orthoText = "Ortho";
+
+    void Start()
+    {
+        cam = Camera.main;
+        camObj = cam.gameObject;
+
+        nearClipEdit.text = cam.nearClipPlane.ToString();
+        farClipEdit.text = cam.farClipPlane.ToString();
+    }
 
     public override void setupGui()
     {   
+        this.projDropdown.onValueChanged.AddListener(delegate{
+            this.onProjectionChange(this.projDropdown);
+        });
+
+        fovSlider.onValueChanged.AddListener(delegate {onFovChange();});
+        nearClipEdit.onValueChanged.AddListener(delegate {onNearClipChange(); });
+        farClipEdit.onValueChanged.AddListener(delegate {onFarClipChange(); });
     }
 
-
-
-    private void onOrthoChange()
+    private void onProjectionChange(Dropdown dropdown) 
     {
-        bool isOrtho = false;
-        this.gameObject.GetComponent<Camera>().orthographic = isOrtho;
+        string selectedProj = dropdown.captionText.text;
+        bool isOrtho = selectedProj.Contains(orthoText);
+        cam.orthographic = isOrtho;
+        fovSlider.interactable = !isOrtho;
+    }
+
+    private void onFovChange()
+    {
+        cam.fieldOfView = fovSlider.value;
+    }
+
+    private void onNearClipChange()
+    {
+        cam.nearClipPlane = float.Parse(nearClipEdit.text);
+    }
+
+    private void onFarClipChange()
+    {
+        cam.farClipPlane = float.Parse(farClipEdit.text);
     }
 }
